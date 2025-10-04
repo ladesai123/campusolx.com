de6400c03b762c69cb0f0ece1706a466c9f7b85d
ladesai123 <ladesaiteja24@gmail.com>
2025-10-04 19:33:17 +0530

    atuoprompt reverted!

--- src/lib/onesignal.ts
+++ src/lib/onesignal.ts
@@ -17,7 +17,6 @@ export function initOneSignal() {
             appId: process.env.NEXT_PUBLIC_ONESIGNAL_APP_ID,
             notifyButton: { enable: true },
             allowLocalhostAsSecureOrigin: true,
-            autoPrompt: false,
           });
         });
       };
