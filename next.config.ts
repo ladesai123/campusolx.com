cdaebe225e1c28ab6de827b8f8a99a5dbf8cdf21
ladesai123 <ladesaiteja24@gmail.com>
2026-05-01 05:38:04 +0530

    chore: whitelist github domains for next/image

--- next.config.ts
+++ next.config.ts
@@ -33,6 +33,14 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "ui-avatars.com",
       },
+      {
+        protocol: "https",
+        hostname: "github.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
     ],
   },
   experimental: {
